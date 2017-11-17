e4a1f394a6d018c672b17431adc66b32f4763537
BERTRAND Guillaume <guillaume5524@hotmail.fr>
2017-11-17 09:59:08 +0100

    ::mailing is working

--- src/main/java/Principale.java
+++ src/main/java/Principale.java
@@ -1,3 +1,13 @@
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class Principale {
     public static void main(String[] args){
         FileLogger fileLogger = new FileLogger();
@@ -5,5 +15,57 @@ public class Principale {
 
         fileLogger.log("bouh");
         consoleLogger.log("bouh");
+
+        // Recipient's email ID needs to be mentioned.
+        String to = "guillaume5524@gmail.com";
+
+        // Sender's email ID needs to be mentioned
+        String from = "guillaume5524@gmail.com";
+        final String username = "75f1ea16e05565";//change accordingly
+        final String password = "06551260b74810";//change accordingly
+
+        // Assuming you are sending email through relay.jangosmtp.net
+        String host = "smtp.mailtrap.io";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "25");
+
+        // Get the Session object.
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+                        return new javax.mail.PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+            // Create a default MimeMessage object.
+            Message message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to));
+
+            // Set Subject: header field
+            message.setSubject("Testing Subject");
+
+            // Now set the actual message
+            message.setText("Hello, this is sample for to check send " +
+                    "email using JavaMailAPI ");
+
+            // Send message
+            Transport.send(message);
+
+            System.out.println("Sent message successfully....");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
